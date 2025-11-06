ae5eefa7378ddfb42b8fbaa89297d724d69eed65
Alessandrau005 <157076152+Alessandra005@users.noreply.github.com>
2025-11-05 22:10:17 -0500

    Update styling in Profile.tsx

--- witcon-frontend/src/pages/Profile.tsx
+++ witcon-frontend/src/pages/Profile.tsx
@@ -35,7 +35,7 @@ export default function Profile() {
 
   const [attendeeData, setAttendeeData] = useState<AttendeeData>({
     firstName: 'Ariana',
-    lastName: 'Casas',
+    lastName: 'De las Casas',
     email: 'aricasa@gmail.com',
     school: 'Harvard University',
     fieldOfStudy: 'Computer Science',
@@ -120,6 +120,7 @@ export default function Profile() {
     const formData = new FormData();
     formData.append('resume', file);
 
+
     try {
       const res = await fetch(`${API_URL}/attendees/${userId}/`, {
         method: 'PATCH',
@@ -144,7 +145,6 @@ export default function Profile() {
     logout(); // clears the userId in context
   };
 
-
   // if (!userId) {
   //   return (
   //     <section className="space-y-4">
@@ -159,6 +159,171 @@ export default function Profile() {
   // if (loading) return <p>Loading profile...</p>;
   // if (error) return <p className="text-red-600">Error: {error}</p>;
 
+
+  function InfoSection({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="break-inside-avoid mb-6 bg-[color:var(--color-tertiary-yellow)] p-6 rounded-xl shadow-md space-y-4">
+      {children}
+    </div>
+  );
+} 
+
+{/* Academic Info Box Component */}
+const AcademicInfoBox = (
+  <InfoSection>
+    {/* Field of Study */}
+    <div className="flex items-center gap-4">
+      <label className="text-[color:var(--color-primary-brown)] font-medium min-w-[100px]">Major:</label>
+      <input
+        type="text"
+        value={attendeeData.fieldOfStudy || ""}
+        readOnly
+        className="flex-1 px-4 py-2 rounded-4xl bg-white text-[color:var(--color-primary-brown)] font-[Actor]"
+      />
+    </div>
+
+    {/* School */}
+    <div className="flex items-center gap-4">
+      <label className="text-[color:var(--color-primary-brown)] font-medium min-w-[100px]">School:</label>
+      <input
+        type="text"
+        value={attendeeData.school || attendeeData.schoolOther || ""}
+        readOnly
+        className="flex-1 px-4 py-2 rounded-4xl bg-white text-[color:var(--color-primary-brown)] font-[Actor]"
+      />
+    </div>
+
+    {/* Level of Study */}
+    <div className="flex items-center gap-4">
+      <label className="text-[color:var(--color-primary-brown)] font-medium min-w-[100px]">Level of Study:</label>
+      <input
+        type="text"
+        value={attendeeData.levelOfStudy || ""}
+        readOnly
+        className="flex-1 px-4 py-2 rounded-4xl bg-white text-[color:var(--color-primary-brown)] font-[Actor]"
+      />
+    </div>
+  </InfoSection>
+);
+
+{/* User Resume and Socials */}
+const ResumeSocialBox = (
+  <InfoSection>
+    {/* Resume Upload Section */}
+    <div className="flex items-center gap-4">
+      <img src="src/images/pdfIcon.png" alt="PDF Icon" className="w-20 h-20" />
+      <p className="text-sm text-[color:var(--color-primary-brown)] font-[Actor] text-center w-full">
+        You only have a limit of 2 more resume uploads.
+      </p>
+    </div>
+
+    {/* LinkedIn */}
+    <div className="flex items-center gap-4">
+      <label className="text-[color:var(--color-primary-brown)] font-medium min-w-[100px]">LinkedIn:</label>
+      <input
+        type="text"
+        value={attendeeData.linkedin || ""}
+        readOnly
+        className="flex-1 px-4 py-2 rounded-full bg-white text-[color:var(--color-primary-brown)] font-[Actor] w-fit min-w-[150px]"
+      />
+    </div>
+
+    {/* GitHub */}
+    <div className="flex items-center gap-4">
+      <label className="text-[color:var(--color-primary-brown)] font-medium min-w-[100px]">GitHub:</label>
+      <input
+        type="text"
+        value={attendeeData.github || ""}
+        readOnly
+        className="flex-1 px-4 py-2 rounded-full bg-white text-[color:var(--color-primary-brown)] font-[Actor] w-fit min-w-[150px]"
+      />
+    </div>
+
+    {/* Discord */}
+    <div className="flex items-center gap-4">
+      <label className="text-[color:var(--color-primary-brown)] font-medium min-w-[100px]">Discord Username:</label>
+      <input
+        type="text"
+        value={attendeeData.discord || ""}
+        readOnly
+        className="flex-1 px-4 py-2 rounded-full bg-white text-[color:var(--color-primary-brown)] font-[Actor] w-fit min-w-[150px]"
+      />
+    </div>
+  </InfoSection>
+);
+
+  {/*WiCS Resources*/}
+  const WiCSResourcesBox = (
+  <InfoSection>
+    <h3 className="font-semibold text-lg text-[color:var(--color-primary-brown)]">Make the best of WiTCON</h3>
+    {/* WiTCON ‘26 Attendee Guide */}
+    <div className="flex items-center gap-3">
+      <img src="src/images/notionIcon.png" alt="Notion Icon" className="w-6 h-6" />
+      <a
+        href="https://www.notion.so/WiTCON-2026-Attendee-Guide"
+        target="_blank"
+        className="w-full px-4 py-2 rounded-4xl bg-white text-[color:var(--color-primary-brown)] font-[Actor]"
+      >
+        WiTCON ‘26 Attendee Guide
+      </a>
+    </div>
+
+    {/* Discord */}
+    <div className="flex items-center gap-3">
+      <img src="src/images/discordIcon.png" alt="Discord Icon" className="w-6 h-6" />
+      <a
+        href="https://discord.gg/wicsfiu"
+        target="_blank"
+        className="w-full px-4 py-2 rounded-4xl bg-white text-[color:var(--color-primary-brown)] font-[Actor]"
+      >
+        WiCS Discord
+      </a>
+    </div>
+
+    {/* LinkedIn */}
+    <div className="flex items-center gap-3">
+      <img src="src/images/linkedInIcon.png" alt="LinkedIn Icon" className="w-6 h-6" />
+      <a
+        href="https://www.linkedin.com/company/wicsatfiu/"
+        target="_blank"
+        className="w-full px-4 py-2 rounded-4xl bg-white text-[color:var(--color-primary-brown)] font-[Actor]"
+      >
+        WiCS LinkedIn
+      </a>
+    </div>
+
+    {/* Instagram */}
+    <div className="flex items-center gap-3">
+      <img src="src/images/instagramIcon.png" alt="Instagram Icon" className="w-6 h-6" />
+      <a
+        href="https://instagram.com/wicsfiu"
+        target="_blank"
+        className="w-full px-4 py-2 rounded-4xl bg-white text-[color:var(--color-primary-brown)] font-[Actor]"
+      >
+        WiCS Instagram
+      </a>
+    </div>
+  </InfoSection>
+);
+
+{/* Report an Incident Box */}
+const ReportIncidentBox = (
+  <InfoSection>
+    <h3 className="font-semibold text-2xl text-[color:var(--color-primary-brown)]">REPORT AN INCIDENT</h3>
+    <p className="text-sm text-gray-700">
+      Please, if you feel uncomfortable or witness inappropriate behavior at WiTCON. Please report it using the link below.
+    </p>
+    <a
+      href="https://discord.gg/wicsfiu"
+      target="_blank"
+      className="inline-block bg-[color:var(--color-primary-pink)] text-[color:var(--color-tertiary-yellow)] px-4 py-2 rounded-full hover:bg-pink-700 transition"
+    >
+      REPORT
+    </a>
+  </InfoSection>
+);
+
+
 return (
   <main className="max-w-6xl mx-auto my-10 px-4 space-y-10 font-[Actor]">
   <section className="mb-10">
@@ -167,7 +332,7 @@ return (
       <div className="flex items-center gap-4">
         <div className="w-28 h-28 rounded-full border-4 border-[color:var(--color-primary-pink)] overflow-hidden">
           <img
-            src={`/images/${attendeeData.profileImage || "profilePic1.png"}`}
+            src={`src/images/${attendeeData.profileImage || "profilePic1.png"}`}
             alt="Profile Icon"
             className="w-full h-full object-cover"
           />
@@ -196,7 +361,7 @@ return (
       {/* Right: Edit Button */}
       <button onClick={handleEdit} className="mt-4 md:mt-0">
         <img
-          src="/images/editIcon.png"
+          src="src/images/editIcon.png"
           alt="Edit"
           className="w-8 h-8 hover:scale-105 transition-transform"
         />
@@ -204,160 +369,12 @@ return (
     </div>
     </section>
 
-    <section className="space-y-8 px-4 py-6grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-8 items-start">
-    {/* Academic Info */}
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-8 items-start">
-      <div className="bg-[color:var(--color-tertiary-yellow)] p-8 rounded-xl shadow-md flex flex-col justify-between space-y-4">
-        {/* Field of Study */}
-        <div className="flex items-center gap-4">
-            <label className="text-[color:var(--color-primary-brown)] font-medium min-w-[100px]">Major:</label>
-            <div className="flex-1">
-              <input
-                type="text"
-                value={attendeeData.fieldOfStudy || ""}
-                readOnly
-                className="w-full px-4 py-2 rounded-4xl bg-white text-[color:var(--color-primary-brown)] font-[Actor]"
-              />
-            </div>
-          </div>
-        {/* School */}
-        <div className="flex items-center gap-4">
-            <label className="text-[color:var(--color-primary-brown)] font-medium min-w-[100px]">School:</label>
-            <div className="flex-1">
-              <input
-                type="text"
-                value={attendeeData.school || attendeeData.schoolOther || ""}
-                readOnly
-                className="w-full px-4 py-2 rounded-4xl bg-white text-[color:var(--color-primary-brown)] font-[Actor]"
-              />
-            </div>
-          </div>
-        {/* Level of Study */}
-        <div className="flex items-center gap-4">
-            <label className="text-[color:var(--color-primary-brown)] font-medium min-w-[100px]">Level of Study:</label>
-            <div className="flex-1">
-              <input
-                type="text"
-                value={attendeeData.levelOfStudy || ""}
-                readOnly
-                className="w-full px-4 py-2 rounded-4xl bg-white text-[color:var(--color-primary-brown)] font-[Actor]"
-              />
-            </div>
-          </div>
-      </div>
-
-      {/* Resume & Socials */}
-      <div className="bg-[color:var(--color-tertiary-yellow)] p-6 rounded-xl shadow-md space-y-4">
-        {/* Resume Upload Section */}
-        <div className="flex items-center gap-4">
-          <img src="/images/pdfIcon.png" alt="PDF Icon" className="w-20 h-20" />
-            <p className="text-sm text-[color:var(--color-primary-brown)] font-[Actor] text-center w-full">
-              You only have a limit of 2 more resume uploads.
-            </p>
-        </div>
-
-        {/* LinkedIn */}
-        <div className="flex items-center gap-4">
-          <label className="text-[color:var(--color-primary-brown)] font-medium min-w-[100px]">LinkedIn:</label>
-            <input
-              type="text"
-              value={attendeeData.linkedin || ""}
-              readOnly
-              className="flex-1 px-4 py-2 rounded-full bg-white text-[color:var(--color-primary-brown)] font-[Actor] w-fit min-w-[150px]"
-            />
-        </div>
-        {/* GitHub */}
-        <div className="flex items-center gap-4">
-          <label className="text-[color:var(--color-primary-brown)] font-medium min-w-[100px] w-fit min-w-[100px]">GitHub:</label>
-          <input
-            type="text"
-            value={attendeeData.github || ""}
-            readOnly
-            className="flex-1 px-4 py-2 rounded-full bg-white text-[color:var(--color-primary-brown)] font-[Actor] w-fit min-w-[150px]"
-          />
-        </div>
-
-        {/* Discord */}
-        <div className="flex items-center gap-4">
-          <label className="text-[color:var(--color-primary-brown)] font-medium min-w-[100px]">Discord Username:</label>
-          <input
-            type="text"
-            value={attendeeData.discord || ""}
-            readOnly
-            className="flex-1 px-4 py-2 rounded-full bg-white text-[color:var(--color-primary-brown)] font-[Actor] w-fit min-w-[150px]"
-          />
-        </div>
-      </div>
-
-      
-      {/* Witcon Resources */}
-      <div className="bg-[color:var(--color-tertiary-yellow)] p-6 rounded-xl shadow-md space-y-4">
-        <h3 className="font-semibold text-lg text-[color:var(--color-primary-brown)]">Make the best of WiTCON</h3>
-        
-        {/* WiTCON  ‘26 Attendee Guide  */}
-          <div className="flex items-center gap-3">
-            <img src="/images/notionIcon.png" alt="Notion Icon" className="w-6 h-6" />
-              <a
-                href="https://www.notion.so/WiTCON-2026-Attendee-Guide"
-                target="_blank"
-                className="w-full px-4 py-2 rounded-4xl bg-white text-[color:var(--color-primary-brown)] font-[Actor]"
-              >
-                WiTCON  ‘26 Attendee Guide 
-              </a>
-          </div>  
-
-        {/* Discord */}
-          <div className="flex items-center gap-3">
-            <img src="/images/discordIcon.png" alt="Discord Icon" className="w-6 h-6" />
-              <a
-                href="https://discord.gg/wicsfiu"
-                target="_blank"
-                className="w-full px-4 py-2 rounded-4xl bg-white text-[color:var(--color-primary-brown)] font-[Actor]"
-              >
-                WiCS Discord
-              </a>
-          </div>        
-        
-        {/* LinkedIn */}
-          <div className="flex items-center gap-3">
-            <img src="/images/linkedInIcon.png" alt="Linkedin Icon" className="w-6 h-6" />
-              <a
-                href="https://www.linkedin.com/company/wicsatfiu/"
-                target="_blank"
-                className="w-full px-4 py-2 rounded-4xl bg-white text-[color:var(--color-primary-brown)] font-[Actor]"
-              >
-                WiCS LinkedIn
-              </a>
-          </div>
-        {/* Instagram */}
-          <div className="flex items-center gap-3">
-            <img src="/images/instagramIcon.png" alt="Instagram Icon" className="w-6 h-6" />
-              <a
-                href="https://instagram.com/wicsfiu"
-                target="_blank"
-                className="w-full px-4 py-2 rounded-4xl bg-white text-[color:var(--color-primary-brown)] font-[Actor]"
-              >
-                WiCS Instagram
-              </a>
-          </div>
-      </div>
-
-         
-      {/* REPORT AN INCIDENT box */}
-      <div className="bg-[color:var(--color-tertiary-yellow)] p-6 rounded-xl shadow-md space-y-4">
-
-        <h3 className="font-semibold text-2xl text-[color:var(--color-primary-brown)]">REPORT AN INCIDENT</h3>
-        <p className="text-sm text-gray-700">
-          Please, if you feel uncomfortable or witness inappropriate behavior at WiTCON. Please report it using the link below.
-        </p>
-          <a
-            href="https://discord.gg/wicsfiu"
-            target="_blank"
-            className="inline-block bg-[color:var(--color-primary-pink)] text-[color:var(--color-tertiary-yellow)] px-4 py-2 rounded-full hover:bg-pink-700 transition"
-          >
-            REPORT
-        </a>
-      </div>
+    <section>
+    <div className="columns-1 md:columns-2 gap-6">
+      {AcademicInfoBox}
+      {WiCSResourcesBox}
+      {ResumeSocialBox}
+      {ReportIncidentBox}
     </div>
   </section>
   </main>
